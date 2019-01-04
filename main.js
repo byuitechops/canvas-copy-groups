c249fa71a274a6de12516cca9a5fa3a80c23a270
Seth Childers <sethchilders92@gmail.com>
2019-01-04 16:34:59 -0700

    Added deleteExistingMatches

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const retrieveGroupData = require('./modules/retrieveGroupData.js');
+const deleteExistingMatches = require('./modules/deleteExistingMatches.js');
 const createCategories = require('./modules/createCategories.js');
 const createGroups = require('./modules/createGroups.js');
 const associateAssignments = require('./modules/associateAssignments.js');
@@ -8,34 +9,31 @@ const Logger = require('logger');
 const logger = new Logger('Group Copy');
 
 module.exports = (sourceCourseID, targetCourseID, deleteDefaultCategory) => {
-    return new Promise((resolve, reject) => {
-        retrieveGroupData(sourceCourseID, logger)
-            .then(groupData => {
-                return createCategories(sourceCourseID, targetCourseID, groupData, logger);
-            })
-            .then(groupData => {
-                return createGroups(sourceCourseID, targetCourseID, groupData, logger);
-            })
-            .then(groupData => {
-                return associateAssignments(sourceCourseID, targetCourseID, groupData, logger);
-            })
-            .then(groupData => {
-                return associateDiscussions(sourceCourseID, targetCourseID, groupData, logger);
-            })
-            .then(groupData => {
-                if (deleteDefaultCategory === true) {
-                    return deleteProjectGroups(sourceCourseID, targetCourseID, groupData, logger);
-                }
-                return {groupData, logger};
-            })
-            .then(({groupData, logger}) =>{
-                logger.htmlReport('./htmlReport');
-                logger.jsonReport('./jsonReport');
-                return groupData;
-            })
-            .then(resolve)
-            .catch(reject);
-    });
-
-
+    retrieveGroupData(sourceCourseID, logger)
+        .then(groupData => {
+            return deleteExistingMatches(sourceCourseID, targetCourseID, groupData, logger);
+        })
+        .then(groupData => {
+            return createCategories(sourceCourseID, targetCourseID, groupData, logger);
+        })
+        .then(groupData => {
+            return createGroups(sourceCourseID, targetCourseID, groupData, logger);
+        })
+        .then(groupData => {
+            return associateAssignments(sourceCourseID, targetCourseID, groupData, logger);
+        })
+        .then(groupData => {
+            return associateDiscussions(sourceCourseID, targetCourseID, groupData, logger);
+        })
+        .then(groupData => {
+            if (deleteDefaultCategory === true) {
+                return deleteProjectGroups(sourceCourseID, targetCourseID, groupData, logger);
+            }
+            return {groupData, logger};
+        })
+        .catch((e)=> console.error(e))
+        .finally(() =>{
+            logger.htmlReport('./htmlReport');
+            logger.jsonReport('./jsonReport');
+        });
 };
---
+++ modules/deleteExistingMatches.js
@@ -0,0 +1,34 @@
+const canvas = require('canvas-api-wrapper');
+
+module.exports = (sourceCourseID, targetCourseID, groupData, logger) => {
+
+    async function removeMatchingCategories() {
+        // get the categories from each course
+        let targetCategories = await canvas.get(`/api/v1/courses/${targetCourseID}/group_categories`);
+        let sourceCategories = await canvas.get(`/api/v1/courses/${sourceCourseID}/group_categories`);
+        
+        // find the ones that match
+        let categoriesToDelete = sourceCategories.reduce((acc, sourceCategory) => {
+            targetCategories.forEach(targetCategory => {
+                if (sourceCategory.name === targetCategory.name) {
+                    // must be targetCategory for the correct id, or you will be deleting from the source course
+                    acc.push(targetCategory);
+                }
+            })
+            return acc;
+        }, []);
+
+        // delete the ones that match from the targetCourse
+        for (let i = 0; i < categoriesToDelete.length; i++) {
+            await canvas.delete(`/api/v1/group_categories/${categoriesToDelete[i].id}`);
+            logger.log('Deleted Category from course', {
+                categoryId: categoriesToDelete[i].id,
+                categoryName: categoriesToDelete[i].name,
+                courseId: targetCourseID
+            });
+        }
+        return groupData;
+    }
+    
+    return removeMatchingCategories();
+};
